f7ed79d94221a6061dfcfed853ab8b1ba1b09637
Joe Woodson <contact@joewoodson.de>
2017-04-16 21:37:50 +0200

    remove old jest tests

--- src/shared/routes.test.js
+++
@@ -1,6 +0,0 @@
-import { helloEndpointRoute } from './routes'
-
-test('helloEndpointRoute', () => {
-  expect(helloEndpointRoute()).toBe('/ajax/hello/:num')
-  expect(helloEndpointRoute(13)).toBe('/ajax/hello/13')
-})
