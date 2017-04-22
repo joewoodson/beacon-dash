3ea7105dfcc4b9c6fbb0a4aaa3efaab2c25047be
Joe <joe@appstretto.com>
2017-04-22 23:31:11 +0200

    change app name

--- src/shared/config.js
+++ src/shared/config.js
@@ -4,7 +4,7 @@ export const WEB_PORT = process.env.PORT || 8000
 export const WDS_PORT = 7000
 
 export const STATIC_PATH = '/static'
-export const APP_NAME = 'Modern JS Stack'
+export const APP_NAME = 'Beacon Dashboard'
 export const APP_CONTAINER_CLASS = 'js-app'
 export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`
 
