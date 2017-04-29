a2ff98a6db33a03bf7b575d879a697a3a173d2ff
Joe Woodson <contact@joewoodson.de>
2017-04-29 20:45:12 +0200

    configure express to use express-session and passport for auth

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,20 @@
 // @flow
 
+import bodyParser from 'body-parser'
 import compression from 'compression'
+import cookieParser from 'cookie-parser'
 import express from 'express'
+import flash from 'express-flash'
+import passport from 'passport'
+
 import { Server } from 'http'
+import session from 'express-session'
 
 import routing from './routing'
 import { WEB_PORT, STATIC_PATH } from '../shared/config'
 import { isProd } from '../shared/util'
+
+const path = require('path')
 
 const app = express()
 // flow-disable-next-line
@@ -15,6 +23,21 @@ const http = Server(app)
 app.use(compression())
 app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
+
+// config for passport authentication
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// uncomment if using express-session
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(express.static(path.join(__dirname, '..', '..', 'client')))
 
 routing(app)
 
