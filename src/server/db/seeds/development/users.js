7ae233fbf5d1199b0a156d704c148ef81d4a131d
Joe Woodson <contact@joewoodson.de>
2017-04-30 23:53:36 +0200

    fix linting errors

--- src/server/db/seeds/development/users.js
+++ src/server/db/seeds/development/users.js
@@ -1,15 +1,14 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs')
 
-exports.seed = (knex, Promise) => {
-  return knex('users').del()
+exports.seed = (knex, Promise) =>
+  knex('users').del()
   .then(() => {
-    const salt = bcrypt.genSaltSync();
-    const hash = bcrypt.hashSync('test', salt);
+    const salt = bcrypt.genSaltSync()
+    const hash = bcrypt.hashSync('test', salt)
     return Promise.join(
       knex('users').insert({
         username: 'joe',
-        password: hash
-      })
-    );
-  });
-};
+        password: hash,
+      }),
+    )
+  })
