1a5999d600ac29dc7f4e9cf7a60cb524be8590dd
Joe Woodson <contact@joewoodson.de>
2017-05-01 00:38:19 +0200

    use env vars for database urls and add knexfile to repo

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL_TEST,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL_DEV,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds/production'
+    }
+  }
+};
